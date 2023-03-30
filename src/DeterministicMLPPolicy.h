a151a16d56a942c996620278e7155c1fee862cd6
Kevin C.C. Chang <benlkchang@gmail.com>
2023-03-30 15:47:46 -0500

    Added policy.h and constructor for deterministicmlppolicy

--- src/DeterministicMLPPolicy.h
+++ src/DeterministicMLPPolicy.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "Policy.h"
-class DeterministicMLPPolicy: Policy {
+#include "LayerPowered.h"
+class DeterministicMLPPolicy: protected Policy, protected LayerPowered {
     public:
-        
+        DeterministicMLPPolicy(EnvSpec env_spec, const std::vector<int> hidden_sizes = std::vector<int>({32, 32}), std::string hidden_nonlinearity = "rectify", std::string hidden_W_init = "HeUniform", std::tuple<std::string, double> hidden_b_init = std::make_tuple("Constant", 0.), std::string output_nonlinearity = "tanh", std::tuple<std::string, double, double> output_W_init = std::make_tuple("Uniform", -0.003, 0.003), std::tuple<std::string, double, double> output_b_init = std::make_tuple("Uniform", -0.003, 0.003), bool bn = false);
     
         
         
--- src/Policy.h
+++ src/Policy.h
@@ -1,11 +1,23 @@
 #pragma once
 #include "EnvSpec.h"
+#include "Parameterized.h"
+#include "Placeholder.h"
 //SOURCE: https://gitlab.engr.illinois.edu/jundayu2/nsa-nfm/-/blob/master/src/rllab/rllab/policies/base.py
-class Policy {
+class Policy: protected Parameterized {
     public:
         Policy(EnvSpec env_spec): env_spec_(env_spec) {};
 
         virtual torch::Tensor get_action(const torch::Tensor& observation);
+        virtual void reset();
+        Space* observation_space() { return env_spec_.observation_space(); }
+        Space* action_space() { return env_spec_.action_space(); }
+        //Indicates whether the policy is recurrent
+        bool recurrent() {return false; }
+        //Placeholder should be the type of paths
+        void log_diagnostics(const std::vector<Placeholder>& paths) { return; }
+        std::vector<std::string> state_info_keys() {return std::vector<std::string>(); }
+        // "Clean up operation," not sure where this is actually implemented
+        void terminate() {return;}
     protected:
         EnvSpec env_spec_;
 
