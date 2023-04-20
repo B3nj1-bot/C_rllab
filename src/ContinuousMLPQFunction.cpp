f7fd24cad6974b52dc52c1b578218973425fa958
Kevin C.C. Chang <benlkchang@gmail.com>
2023-04-20 18:21:17 -0500

    Made edits to DeterministicMLPPolicy

--- src/ContinuousMLPQFunction.cpp
+++ src/ContinuousMLPQFunction.cpp
@@ -1,41 +1,29 @@
 #include "ContinuousMLPQFunction.h"
 ContinuousMLPQFunction::ContinuousMLPQFunction(EnvSpec env_spec, const std::vector<int> hidden_sizes, std::string hidden_nonlinearity, std::string hidden_W_init, std::tuple<std::string, double> hidden_b_init, int action_merge_layer, std::string output_nonlinearity, std::tuple<std::string, double, double, double> output_W_init, std::tuple<std::string, double, double, double> output_b_init, bool bn) {
-    torch::nn::Linear l_obs(torch::nn::LinearOptions(env_spec.observation_space()->flat_dim(), hidden_sizes[0]).bias(false));
-    torch::nn::Linear l_action(torch::nn::LinearOptions(env_spec.action_space()->flat_dim(), hidden_sizes[0]).bias(false));
-    size_t n_layers = hidden_sizes.size() + 1;
+    //torch::nn::Linear l_obs(torch::nn::LinearOptions(env_spec.observation_space()->flat_dim(), hidden_sizes[0]));
+    
+    //FIX, FIND REPLACEMENT FOR l_action SINCE IT ISNT REALLY AN INPUT LAYER
+    //torch::nn::Linear l_action(torch::nn::LinearOptions(env_spec.action_space()->flat_dim(), hidden_sizes[0]));
+
+    size_t n_layers = hidden_sizes.size();
     if (n_layers > 1) {
         action_merge_layer = ((action_merge_layer % n_layers) + n_layers) % n_layers;
     } else {
         action_merge_layer = 1;
     }
 
-    if (bn) {
-        //maybe not 3d
-        layers = torch::nn::Sequential(torch::nn::BatchNorm3d(env_spec.observation_space()->flat_dim()), l_obs);
-    } else {
-        layers = torch::nn::Sequential(l_obs);
-    }
     //add if cases for other nonlinearities
     torch::nn::init::NonlinearityType nlinearity = torch::kReLU;
 
-    auto l_hidden = l_obs;
+    std::vector<int> sizes = hidden_sizes;
+    sizes.insert(sizes.begin(), env_spec.observation_space()->flat_dim());
     for (size_t idx = 0; idx < hidden_sizes.size() - 1; idx++) {
-        if (bn) {
-            //might not work since it also applies batchnorm to the last hidden layer but the python version doesn't?
-            layers->push_back(torch::nn::BatchNorm3d(hidden_sizes[idx]));
-        }
-
-        //FIX FIX FIX
+        //FIX FIX FIX, use two sets of sequentials? before and after
         // if (idx == action_merge_layer) {
-            
         //     l_hidden = torch::cat({l_hidden, l_action}, 1);
         // }
-
-
-        if (hidden_nonlinearity == "rectify") {
-            layers->push_back(torch::nn::ReLU());
-        }
-        l_hidden = torch::nn::Linear(hidden_sizes[idx], hidden_sizes[idx + 1]);
+        
+        auto l_hidden = torch::nn::Linear(hidden_sizes[idx], hidden_sizes[idx + 1]);
         if (hidden_W_init == "HeUniform") {
             torch::nn::init::kaiming_uniform_(l_hidden->weight, 1, torch::kFanIn, nlinearity);
         }
@@ -43,5 +31,13 @@ ContinuousMLPQFunction::ContinuousMLPQFunction(EnvSpec env_spec, const std::vect
             torch::nn::init::constant_(l_hidden->bias, std::get<1>(hidden_b_init));
         }
         layers->push_back(l_hidden);
+
+        if (bn) {
+            //might not work since it also applies batchnorm to the last hidden layer but the python version doesn't?
+            layers->push_back(torch::nn::BatchNorm3d(hidden_sizes[idx]));
+        }
+        if (hidden_nonlinearity == "rectify") {
+            layers->push_back(torch::nn::ReLU());
+        }
     }
 }
--- src/DeterministicMLPPolicy.cpp
+++ src/DeterministicMLPPolicy.cpp
@@ -12,27 +12,18 @@ output_b_init: initializer for the biases of output layer, default is Uniform(a,
 bn: if using batch_normalization or not
 */
 DeterministicMLPPolicy::DeterministicMLPPolicy(EnvSpec env_spec, const std::vector<int> hidden_sizes, std::string hidden_nonlinearity, std::string hidden_W_init, std::tuple<std::string, double> hidden_b_init, std::string output_nonlinearity, std::tuple<std::string, double, double, double> output_W_init, std::tuple<std::string, double, double, double> output_b_init, bool bn): Policy(env_spec) {
-    //l_obs = L.InputLayer(shape=(None, env_spec.observation_space.flat_dim))
-    
-
-    //maybe convolutional?
-    torch::nn::Linear l_obs(torch::nn::LinearOptions(env_spec.observation_space()->flat_dim(), hidden_sizes[0]).bias(false));
-    if (bn) {
-        //maybe not 3d
-        layers = torch::nn::Sequential(torch::nn::BatchNorm3d(env_spec_.observation_space()->flat_dim()), l_obs);
-    } else {
-        layers = torch::nn::Sequential(l_obs);
-    }
+    //torch::nn::Linear l_obs(torch::nn::LinearOptions(env_spec.observation_space()->flat_dim(), hidden_sizes[0]);
 
     //add if cases for other nonlinearities
     torch::nn::init::NonlinearityType nlinearity = torch::kReLU;
-    
-    auto l_hidden = l_obs;
-    for (size_t idx = 0; idx < hidden_sizes.size() - 1; idx++) {
-        if (hidden_nonlinearity == "rectify") {
-            layers->push_back(torch::nn::ReLU());
-        }
-        l_hidden = torch::nn::Linear(hidden_sizes[idx], hidden_sizes[idx + 1]);
+
+    std::vector<int> sizes = hidden_sizes;
+    sizes.insert(sizes.begin(), env_spec.observation_space()->flat_dim());
+    //sizes begins with the input sizes and has the output size of each subsequent layer
+
+    for (size_t idx = 0; idx < sizes.size() - 1; idx++) {
+        
+        auto l_hidden = torch::nn::Linear(sizes[idx], sizes[idx + 1]);
         if (hidden_W_init == "HeUniform") {
             //check
             //nlinearity is relu by default, makes gain sqrt(2)
@@ -41,17 +32,16 @@ DeterministicMLPPolicy::DeterministicMLPPolicy(EnvSpec env_spec, const std::vect
         if (std::get<0>(hidden_b_init) == "Constant") {
             torch::nn::init::constant_(l_hidden->bias, std::get<1>(hidden_b_init));
         }
-        if (bn) {
-            layers->push_back(torch::nn::BatchNorm3d(hidden_sizes[idx]));
-        }
         layers->push_back(l_hidden);
+        if (bn) {
+            layers->push_back(torch::nn::BatchNorm3d(sizes[idx]));
+        }
+        if (hidden_nonlinearity == "rectify") {
+            layers->push_back(torch::nn::ReLU());
+        }
     }
-
-    //add other output nonlinearities
-    if (output_nonlinearity == "tanh") {
-        layers->push_back(torch::nn::Tanh());
-    }
-    auto l_output = torch::nn::Linear(hidden_sizes[hidden_sizes.size() - 1], env_spec_.action_space()->flat_dim());
+    //gets the last output size and maps to action space size
+    auto l_output = torch::nn::Linear(sizes[sizes.size() - 1], env_spec_.action_space()->flat_dim());
     if (std::get<0>(output_W_init) == "Uniform") {
         double range = -(std::get<3>(output_W_init) - (sqrt(3) * std::get<2>(output_W_init)));
         torch::nn::init::uniform_(l_output->weight, -range, range);
@@ -61,7 +51,12 @@ DeterministicMLPPolicy::DeterministicMLPPolicy(EnvSpec env_spec, const std::vect
         torch::nn::init::uniform_(l_output->bias, -range, range);
     }
     layers->push_back(l_output);
-    
+
+    //add other output nonlinearities
+    if (output_nonlinearity == "tanh") {
+        layers->push_back(torch::nn::Tanh());
+    }
+
     output_layers_->push_back(layers);
 
     //don't need compile function, can just use layers->forward(input)
